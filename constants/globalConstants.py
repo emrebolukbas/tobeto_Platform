891bb148e475f4f7dd5cd845f8b31d168407f4eb
emrebolukbas <yunusemre.bolukbas@hotmail.com>
2024-01-02 15:52:18 +0300

    constans and data created

---
+++ constants/globalConstants.py
@@ -0,0 +1,5 @@
+BASE_URL = "https://tobeto.com/giris"
+E_MAIL_XPATH = "/html/body/div[1]/div/main/section/div/div/div/div/form/input[1]"
+PASSWORD_XPATH = "/html/body/div[1]/div/main/section/div/div/div/div/form/input[2]"
+LOGIN_BUTTON_XPATH = "//*[@id='__next']/div/main/section/div/div/div/div/form/button"
+MANDATORY_FIELD_XPATH = "//*[@id='__next']/div/main/section/div/div/div/div/form/p"
