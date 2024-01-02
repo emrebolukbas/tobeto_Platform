2801daa0b43bcf4ee9144180ae3afa4adbe7db98
emrebolukbas <yunusemre.bolukbas@hotmail.com>
2024-01-02 16:47:01 +0300

    new constants added

--- constants/globalConstants.py
+++ constants/globalConstants.py
@@ -3,3 +3,6 @@ E_MAIL_XPATH = "/html/body/div[1]/div/main/section/div/div/div/div/form/input[1]
 PASSWORD_XPATH = "/html/body/div[1]/div/main/section/div/div/div/div/form/input[2]"
 LOGIN_BUTTON_XPATH = "//*[@id='__next']/div/main/section/div/div/div/div/form/button"
 MANDATORY_FIELD_XPATH = "//*[@id='__next']/div/main/section/div/div/div/div/form/p"
+LOGIN_PAGE_TOBETO_IMG = "//*[@id='__next']/div/main/section/div/div/div/div/div[1]/span/img"
+FORGOT_PASSWORD = "//*[@id='__next']/div/main/section/div/div/div/div/form/label/small/p"
+SIGN_UP_TEXT = "//*[@id='__next']/div/main/section/div/div/div/div/div[2]/label/small"
