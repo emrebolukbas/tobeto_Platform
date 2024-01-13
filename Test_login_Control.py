9ed2c3dfb7f436ad6229db313dcebb68349b693e
emrebolukbas <yunusemre.bolukbas@hotmail.com>
2024-01-14 00:31:03 +0300

    new cases added

--- Test_login_Control.py
+++ Test_login_Control.py
@@ -31,6 +31,7 @@ class Test_tobetoPlatformLogin():
 
         return data
     
+    
     #1)Giriş yap alanı görüntülenebilir ve işlevselliği test edilecektir.
     def test_visibility_of_login_page(self):
         tobeto_Img = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,c.LOGIN_PAGE_TOBETO_IMG)))
@@ -53,9 +54,9 @@ class Test_tobetoPlatformLogin():
         errorMessage = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,c.MANDATORY_FIELD_XPATH)))
         assert errorMessage.text == "Doldurulması zorunlu alan*"
     
-    #3Kullanıcının e-posta ve şifre bilgilerini yanlış girerek sisteme giriş yapması  test edilecektir.
+    #3)Kullanıcının e-posta ve şifre bilgilerini yanlış girerek sisteme giriş yapması  test edilecektir.
     @pytest.mark.parametrize("email_param, password_param", getData())
-    def test_invlalidLogin(self,email_param,password_param):
+    def test_invalidLogin(self,email_param,password_param):
         eMail = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,c.E_MAIL_XPATH)))
         eMail.send_keys(email_param)
         password = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,c.PASSWORD_XPATH)))
@@ -66,28 +67,76 @@ class Test_tobetoPlatformLogin():
         systemMessage = WebDriverWait(self.driver,2).until(ec.presence_of_element_located((By.XPATH,c.SYSTEM_MESSAGE)))
         #assert systemMessage.text == "• Geçersiz e-posta veya şifre."
         assert "Geçersiz e-posta veya şifre." in systemMessage.text
-         
-"""     def test_password_passing_empty(self):
+
+    #4)Giriş yap alanı görüntülenebilir ve işlevselliği test edilecektir.
+    def test_button_control(self):
         eMail = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,c.E_MAIL_XPATH)))
-        eMail.send_keys("test@test.com")
         password = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,c.PASSWORD_XPATH)))
-        password.send_keys("")    
         loginButton = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,c.LOGIN_BUTTON_XPATH)))
-        loginButton.click()
-        errorMessage = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,c.MANDATORY_FIELD_XPATH)))
-        assert errorMessage.text == "Doldurulması zorunlu alan*"
-     
-    def test_email_passing_empty(self):
+        forgotPassword = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='__next']/div/main/section/div/div/div[1]/div/form/label/small/p")))
+        register = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='__next']/div/main/section/div/div/div[1]/div/div[2]/label/small/a")))
+        tobetoLogo = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='__next']/div/main/section/div/div/div[1]/div/div[1]/span/img")))
+
+        #Bu kısımda butonların görünürlüğü teyit edilmektedir.
+        assert eMail.is_enabled() and password.is_enabled() and loginButton.is_enabled() and forgotPassword.is_enabled() and register.is_enabled() and tobetoLogo.is_enabled()
+
+    #5) Kullanıcının sistemde kayıtlı e-posta ve şifre bilgileriyle  giriş yapabilmesi test edilecektir.
+    @pytest.mark.parametrize("email, password", [("fogacap180@ubinert.com", "abc123456")])
+    def test_valid_login(self,email,password):
         eMail = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,c.E_MAIL_XPATH)))
-        eMail.send_keys("")
-        password = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,c.PASSWORD_XPATH)))
-        password.send_keys("testSifre")    
-        loginButton = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,c.LOGIN_BUTTON_XPATH)))
+        eMail.send_keys(email)
+        Password = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,c.PASSWORD_XPATH)))
+        Password.send_keys(password)
+        loginButton = self.driver.find_element(By.XPATH,c.LOGIN_BUTTON_XPATH)
         loginButton.click()
-        errorMessage = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,c.MANDATORY_FIELD_XPATH)))
-        assert errorMessage.text == "Doldurulması zorunlu alan*"
- """
+        systemMessage = WebDriverWait(self.driver,2).until(ec.presence_of_element_located((By.XPATH,c.SYSTEM_MESSAGE)))
+        assert  "Giriş başarılı." in systemMessage.text
+
+    #6) Kullanıcının şifremi unuttum butonuna  tıklayarak  ‘’ Şifremi Unuttum ’’ sayfasında geçerli e-posta ile kullanıcının şifre sıfırlaması test edilecektir.
+    def test_valid_reset_password(self):
+        forgotPassword = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='__next']/div/main/section/div/div/div[1]/div/form/label/small/p")))
+        forgotPassword.click()
+        current_url = self.driver.current_url
+        expected_url = "https://tobeto.com/sifremi-unuttum"
+        if (current_url and expected_url):
+            True
+        else:
+            False
+        resetPasswordInput = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='__next']/div/main/section/div/div/div/input")))
+        resetPasswordInput.send_keys("test@tobeto.com")
+        sendButton = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='__next']/div/main/section/div/div/div/button")))
+        sendButton.click()
+        systemMessage = WebDriverWait(self.driver,2).until(ec.presence_of_element_located((By.XPATH,c.SYSTEM_MESSAGE)))
+
+        assert "Şifre sıfırlama linkini e-posta adresinize gönderdik. Lütfen gelen kutunuzu kontrol edin." in systemMessage.text 
+    
+    #7) Kullanıcının şifremi unuttum butonuna  tıklayarak  ‘’ https://tobeto.com/sifremi-unuttum’’ sayfasında geçersiz e-posta ile kullanıcının şifre sıfırlaması test edilecektir.
+    def test_invalid_reset_password(self):
+        forgotPassword = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='__next']/div/main/section/div/div/div[1]/div/form/label/small/p")))
+        forgotPassword.click()
+        current_url = self.driver.current_url
+        expected_url = "https://tobeto.com/sifremi-unuttum"
+        if (current_url and expected_url):
+            True
+        else:
+            False
+        resetPasswordInput = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='__next']/div/main/section/div/div/div/input")))
+        resetPasswordInput.send_keys("test.tobeto.com")
+        sendButton = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='__next']/div/main/section/div/div/div/button")))
+        sendButton.click()
+        systemMessage = WebDriverWait(self.driver,2).until(ec.presence_of_element_located((By.XPATH,c.SYSTEM_MESSAGE)))
+
+        assert "Girdiğiniz e-posta geçersizdir." in systemMessage.text 
+
+
+        
+
+
+                                                                
+
+        
 
 
 
+        
         
