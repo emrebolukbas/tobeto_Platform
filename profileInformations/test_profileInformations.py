be3cc123e733a5152e7c3e18a3aae2a80bb79d89
emrebolukbas <yunusemre.bolukbas@hotmail.com>
2024-03-02 15:44:08 +0300

    new cases added

--- profileInformations/test_profileInformations.py
+++ profileInformations/test_profileInformations.py
@@ -5,6 +5,7 @@ from time import sleep
 from selenium.webdriver.support.wait import WebDriverWait #ilgili driverı bekleten yapı
 from selenium.webdriver.support import expected_conditions as ec #beklenen koşullar
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 import pytest
 import openpyxl
@@ -25,7 +26,7 @@ class Test_profileInformations():
         eMail = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,c.E_MAIL_XPATH)))
         eMail.send_keys("fogacap180@ubinert.com")
         Password = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,c.PASSWORD_XPATH)))
-        Password.send_keys("abc123456")
+        Password.send_keys("abc1234")
         loginButton = self.driver.find_element(By.XPATH,c.LOGIN_BUTTON_XPATH)
         loginButton.click()
         systemMessage = WebDriverWait(self.driver,2).until(ec.presence_of_element_located((By.XPATH,"//*[@id='__next']/div/div[2]/div/div[2]")))
@@ -170,6 +171,197 @@ class Test_profileInformations():
         
         for mardatoryField in mardatoryFields:
             assert "Doldurulması zorunlu alan" in mardatoryField.text
+       
+    #Eğitim Hayatım sayfası görüntüleme test edilecektir.
+    def test_my_education (self):
+        self.driver.get('https://tobeto.com/profilim/profilimi-duzenle/egitim-hayatim')
+        sleep(3)
+        assert "Eğitim" in self.driver.page_source
+
+    #Eğitim Hayatım sayfası boş kayıt test edilecektir.
+    def test_my_education_life(self):
+        self.driver.get('https://tobeto.com/profilim/profilimi-duzenle/egitim-hayatim')
+        save_button_element = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'form > button')))
+        save_button_element.click()
+        sleep(1)
+        assert "Doldurulması zorunlu alan" in self.driver.page_source
+       
+    
+    #Eğitim Hayatım Eğitim durumu kontrol
+    def test_my_education_status_options(self):
+        self.driver.get('https://tobeto.com/profilim/profilimi-duzenle/egitim-hayatim')
+        option_values = []
+        select_element = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'select[name="EducationStatus"]')))
+        select_element.click()
+        select = Select(select_element)
+        for option in select.options:
+            option_values.append(option.get_attribute("value"))
+        option_count = len(select.options)
+        if option_count > 4:
+            message = "Select içinde 4'ten fazla seçenek var!"
+        else:
+            message = "Select içinde 4 veya daha az seçenek var!"
+        print(message)
+        assert 1 == 1, print(message)
+
+    #Eğitim Hayatım üniversite input kontrol
+    def test_my_education_university_input_control(self):
+        self.driver.get('https://tobeto.com/profilim/profilimi-duzenle/egitim-hayatim')
+        university_input_element = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'input[name="University"]')))
+        for i in range(0, 100):
+            university_input_element.send_keys(".")
+        sleep(1)
+        save_button_element = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'form > button')))
+        save_button_element.click()
+        sleep(1)
+        next_element = university_input_element.find_element(By.XPATH, "following-sibling::span")
+        
+        if "En fazla" in next_element.text:
+            print("100 karakter girilemiyor.")
+        else:
+            print("100 karakter girilebiliyor.")
+
+        sleep(1)
+        university_input_element.clear()
+        save_button_element.click()
+        sleep(1)
+        next_element = university_input_element.find_element(By.XPATH, "following-sibling::span")
+        
+        if "zorunlu" in next_element.text:
+            print("Boş bırakılamıyor.")
+        else:
+            print("Boş bırakılabiliyor.")
+        assert "Eğitim" in self.driver.page_source
+
+    #Eğitim Hayatım bölüm input kontrol
+    def test_my_education_department_input_control(self):
+        self.driver.get('https://tobeto.com/profilim/profilimi-duzenle/egitim-hayatim')
+        department_input_element = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'input[name="Department"]')))
+        for i in range(0, 100):
+            department_input_element.send_keys(".")
+        sleep(1)
+        save_button_element = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'form > button')))
+        save_button_element.click()
+        sleep(1)
+        next_element = department_input_element.find_element(By.XPATH, "following-sibling::span")
+        
+        if "En fazla" in next_element.text:
+            print("100 karakter girilemiyor.")
+        else:
+            print("100 karakter girilebiliyor.")
+
+        sleep(1)
+        department_input_element.clear()
+        save_button_element.click()
+        sleep(1)
+        next_element = department_input_element.find_element(By.XPATH, "following-sibling::span")
+        
+        if "zorunlu" in next_element.text:
+            print("Boş bırakılamıyor.")
+        else:
+            print("Boş bırakılabiliyor.")
+        assert "Eğitim" in self.driver.page_source
+      
+    #Eğitim Hayatım başlangıç yılı input kontrol
+    def test_my_education_start_year_input_control(self):
+        self.driver.get('https://tobeto.com/profilim/profilimi-duzenle/egitim-hayatim')
+        start_year_main_element =  WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, '.react-datepicker-wrapper')))
+        start_year_input_element = WebDriverWait(start_year_main_element,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'div > input')))
+        start_year_input_element.send_keys("2024")
+        save_button_element = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'form > button')))
+        save_button_element.click()
+        sleep(2)
+        start_year_input_element.send_keys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE)
+        sleep(1)
+        save_button_element.click()
+        sleep(5)
+        next_element = start_year_main_element.find_element(By.XPATH, "following-sibling::span")
+        if "zorunlu" in next_element.text:
+            print("Boş bırakılamıyor.")
+        else:
+            print("Boş bırakılabiliyor.")
+        assert "Eğitim" in self.driver.page_source
+        
+    #Eğitim Hayatım mezuniyet yılı input kontrol
+    def test_my_education_graduation_year_input_control(self):
+        self.driver.get('https://tobeto.com/profilim/profilimi-duzenle/egitim-hayatim')
+        graduation_year_main_element =  WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, '.react-datepicker-wrapper:nth-child(2)')))
+        graduation_year_input_element = WebDriverWait(graduation_year_main_element,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'div > input')))
+    
+        if graduation_year_input_element.is_enabled():
+            print("Mezuniyet yılı etkin durumda.")
+        else:
+            print("Mezuniyet yılı pasif durumda.")
+        assert "Eğitim" in self.driver.page_source
+        
+    #Eğitim Hayatım devam ediyorum aktifken mezuniyet yılı pasiflik kontrol
+    def test_my_education_checkbox_control(self):
+        self.driver.get('https://tobeto.com/profilim/profilimi-duzenle/egitim-hayatim')
+        sleep(1)
+        datepicker_elements =  WebDriverWait(self.driver,5).until(ec.visibility_of_all_elements_located((By.CSS_SELECTOR, '.react-datepicker-wrapper')))
+        start_year_input_element = WebDriverWait(datepicker_elements[0],5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'div > input')))
+        start_year_input_element.send_keys("2020")
+        sleep(1)
+        graduation_year_main_element = datepicker_elements[1]
+        graduation_year_input_element = WebDriverWait(graduation_year_main_element,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'div > input')))
+        graduation_year_input_element.click()
+        graduation_year_input_element.send_keys("2022")
+        sleep(1)
+        start_year_input_element.click()
+        sleep(1)
+        checkbox_element =  WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.NAME, 'checkbox')))
+        checkbox_element.click()
+        sleep(1)
+        graduation_year_input_element.click()
+        
+        if graduation_year_input_element.is_enabled():
+            print("Mezuniyet yılı etkin durumda.")
+        else:
+            print("Mezuniyet yılı pasif durumda.")
+        sleep(1)
+        assert "Eğitim" in self.driver.page_source
+        
+    #Eğitim Hayatım kayıt kontrol
+    def test_my_education_save_control(self):
+        self.driver.get('https://tobeto.com/profilim/profilimi-duzenle/egitim-hayatim')
+
+        education_status_select_element = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'select[name="EducationStatus"]')))
+        education_status_select_element.click()
+        education_status_select = Select(education_status_select_element)
+        education_status_select.select_by_visible_text("Ön Lisans")
+        sleep(1)
+        
+        university_input_element = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'input[name="University"]')))
+        university_input_element.send_keys("Haliç University")
+        sleep(1)
+        
+        department_input_element = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'input[name="Department"]')))
+        department_input_element.send_keys("Software Tester")
+        sleep(1)
+
+        datepicker_elements =  WebDriverWait(self.driver,5).until(ec.visibility_of_all_elements_located((By.CSS_SELECTOR, '.react-datepicker-wrapper')))
+        start_year_input_element = WebDriverWait(datepicker_elements[0],5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'div > input')))
+        start_year_input_element.click()
+        sleep(1)
+        WebDriverWait(datepicker_elements[1],5).until(ec.visibility_of_element_located((By.XPATH, '//div[text()="2019"]'))).click()
+        sleep(1)
+
+        graduation_year_input_element = WebDriverWait(datepicker_elements[1],5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'div > input')))
+        graduation_year_input_element.click()
+        sleep(1)
+        WebDriverWait(datepicker_elements[1],5).until(ec.visibility_of_element_located((By.XPATH, '//div[text()="2021"]'))).click()
+        sleep(1)
+        
+        save_button_element = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'form > button')))
+        save_button_element.click()
+        sleep(3)
+        try:
+            success_message_element = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR, '.fade.toast.bg-success')))
+            print("Kayıt başarılı")
+        except NoSuchElementException:
+            print("Kayıt başarılı değil")     
+            sleep(2)
+            assert "Eğitim" in self.driver.page_source
 
     #Ayarlar sayfasında eski şifre farklı girilerek şifre değiştirme işlemi test edilecektir.
     def test_Setting_with_Diff_Password_Try(self):
@@ -256,18 +448,18 @@ class Test_profileInformations():
         settings = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='__next']/div/main/section/div/div/div[1]/div/a[8]")))
         settings.click()
         oldPassword = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='__next']/div/main/section/div/div/div[2]/form/div/div[1]/input")))
-        oldPassword.send_keys("abc123456")
+        oldPassword.send_keys("abc1234")
         newPassword = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='__next']/div/main/section/div/div/div[2]/form/div/div[2]/input")))
-        newPassword.send_keys("abc1234")
+        newPassword.send_keys("abc123456")
         newPasswordAgain = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='__next']/div/main/section/div/div/div[2]/form/div/div[3]/input")))
-        newPasswordAgain.send_keys("abc1234")
+        newPasswordAgain.send_keys("abc123456")
         changePassword = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='__next']/div/main/section/div/div/div[2]/div/div/div[1]/button")))
         changePassword.click()
         systemMessage = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='__next']/div/div[2]/div/div[2]"))).text
         sleep(5)
-        oldPassword.send_keys("abc1234")
-        newPassword.send_keys("abc123456")
-        newPasswordAgain.send_keys("abc123456")
+        oldPassword.send_keys("abc123456")
+        newPassword.send_keys("abc1234")
+        newPasswordAgain.send_keys("abc1234")
         assert "Şifreniz güncellenmiştir" in systemMessage
 
     #Sertifikalarım sayfasının görüntülenmesi test edilecektir.
