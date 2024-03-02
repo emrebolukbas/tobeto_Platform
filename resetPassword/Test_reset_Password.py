ae4e8a156aa117b1e1d51d83c258346cb381e24d
emrebolukbas <yunusemre.bolukbas@hotmail.com>
2024-03-02 17:06:56 +0300

    bug test case added

--- resetPassword/Test_reset_Password.py
+++ resetPassword/Test_reset_Password.py
@@ -35,9 +35,28 @@ class Test_resetPassword():
         sendButton.click()
         systemMessage = WebDriverWait(self.driver,2).until(ec.presence_of_element_located((By.XPATH,c.SYSTEM_MESSAGE)))
 
-        assert "Şifre sıfırlama linkini e-posta adresinize gönderdik. Lütfen gelen kutunuzu kontrol edin." in systemMessage.text 
+        assert "Şifre sıfırlama linkini e-posta adresinize gönderdik. Lütfen gelen kutunuzu kontrol edin." in systemMessage.text
+
+    #2) Kullanıcının platformda kayıtlı olmayan rastgele bir e-posta ile şifre sıfırlaması test edilecektir.
+    def test_withRandomMail_reset_password(self):
+        forgotPassword = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='__next']/div/main/section/div/div/div[1]/div/form/label/small/p")))
+        forgotPassword.click()
+        current_url = self.driver.current_url
+        expected_url = "https://tobeto.com/sifremi-unuttum"
+        if (current_url and expected_url):
+            True
+        else:
+            False
+        resetPasswordInput = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='__next']/div/main/section/div/div/div/input")))
+        resetPasswordInput.send_keys("123fd1s3fsfa@test1323eq.deneme")
+        sendButton = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='__next']/div/main/section/div/div/div/button")))
+        sendButton.click()
+        systemMessage = WebDriverWait(self.driver,2).until(ec.presence_of_element_located((By.XPATH,c.SYSTEM_MESSAGE)))
+
+        assert "Girmiş olduğunuz e-posta adresine ait bir kayıt bulunamadı." in systemMessage.text
+        #Gerçekleşen Sonuç: Şifre sıfırlama linkini e-posta adresinize gönderdik. Lütfen gelen kutunuzu kontrol edin. Rastgele mail dışında sadece mail kurallarına yazılan herşey mail olarak algılanmakta ve mail gönderimi gerçekleşmektedir. 
     
-    #2) Kullanıcının şifremi unuttum butonuna  tıklayarak  ‘’ https://tobeto.com/sifremi-unuttum’’ sayfasında geçersiz e-posta ile kullanıcının şifre sıfırlaması test edilecektir.
+    #3) Kullanıcının şifremi unuttum butonuna  tıklayarak  ‘’ https://tobeto.com/sifremi-unuttum’’ sayfasında geçersiz e-posta ile kullanıcının şifre sıfırlaması test edilecektir.
     def test_invalid_reset_password(self):
         forgotPassword = WebDriverWait(self.driver,2).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='__next']/div/main/section/div/div/div[1]/div/form/label/small/p")))
         forgotPassword.click()
